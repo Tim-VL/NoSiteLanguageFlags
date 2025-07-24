51bb36e62c315268eb540f0090cdc5fdcc61267a
Tim <75847795+Tim-VL@users.noreply.github.com>
2025-07-24 22:23:45 +0200

    Update main.js

--- src/Resources/app/administration/main.js
+++ src/Resources/app/administration/main.js
@@ -4,7 +4,7 @@ import './src/module/sw-order/extension/order-list-extension.html.twig';
 const style = document.createElement('style');
 style.innerHTML = `
     .order-language-english-row {
-        background-color: #e6f7ff !important;
+        background-color: #ffc0cb !important;
     }
 `;
 document.head.appendChild(style);
